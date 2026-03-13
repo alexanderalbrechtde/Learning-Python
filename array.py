b1cc505dd5f8afef4cecb0d33053e04dd508c95b
Alexander Albrecht <alexander.albrecht2004@gmail.com>
2026-03-13 13:44:21 +0100

    just some trying

---
+++ array.py
@@ -0,0 +1,13 @@
+def array():
+    fruits = ['apple', 'banana', 'mango']
+    for n in fruits:
+        print (n)
+
+#array()
+
+def square():
+    num = [1,2,3,4,5]
+    for n in num:
+        print (n*n)
+
+#square()
---
+++ while.py
@@ -0,0 +1,8 @@
+def self():
+    a = 5
+    while a <= 10:
+        print (a)
+        a +=1
+
+
+self()
