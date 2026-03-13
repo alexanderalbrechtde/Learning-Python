87f207fb1cae8bc45fc522681ecabb2145237659
Alexander Albrecht <alexander.albrecht2004@gmail.com>
2026-03-13 14:51:28 +0100

    first time using pandas

--- array.py
+++ array.py
@@ -1,22 +1,36 @@
+import pandas as pd
+
+
 def array():
     fruits = ['apple', 'banana', 'mango']
     for n in fruits:
-        print (n)
+        print(n)
 
-#array()
+
+# array()
 
 def square():
-    num = [1,2,3,4,5]
+    num = [1, 2, 3, 4, 5]
     for n in num:
-        print (n*n)
+        print(n * n)
 
-#square()
+
+# square()
 
 def mix():
     num = [1, 2, 3, 4, 5]
     fruits = ['apple', 'banana', 'mango', 'pineapple', 'cherry']
-    #print (len(fruits))
+    # print (len(fruits))
     for f, n in zip(fruits, num):
-            print (n,f)
-            print(len(f))
-mix()
+        print(n, f)
+        print(len(f))
+
+
+# mix()
+
+num = [1, 2, 3, 4, 5]
+fruits = ['apple', 'banana', 'mango', 'pineapple', 'cherry']
+
+df = pd.DataFrame({'Number': num, 'Fruits': fruits})
+
+print(df)
