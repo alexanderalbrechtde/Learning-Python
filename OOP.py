69a2413335a2eb5156a070c7543c3926582dbc6a
Alexander Albrecht <alexander.albrecht2004@gmail.com>
2026-03-19 17:22:00 +0100

    OOP 2

--- OOP.py
+++ OOP.py
@@ -32,9 +32,23 @@ class Human:
     def language(self, language):
         return '{0} is from {1} and speake {2}.'.format(self.name, self.location, language)
 
+    def set_location(self, location):
+        self.location = location
+
+    def get_name(self):
+        return self.name
+
+    def get_age(self):
+        return self.age
+
+    def get_location(self):
+        return self.location
+
 
 alex = Human('Alex', '21', 'Germany')
 
+alex.set_location('Berlin')
+
 print(alex.description())
 print(alex.language('german'))
-
+print(alex.get_name() + ' is here')
